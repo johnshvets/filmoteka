026f67bf56a9a05970f3f317b8d4c669dd3f89ff
antonlazurko <lazurkoanton87@gmail.com>
2020-11-17 20:16:22 +0200

    do example render

--- src/components/local-storage.js
+++ src/components/local-storage.js
@@ -1,8 +1,9 @@
+import tamplate from './templates.hbs';
 // обьект для примера
 const movieObj1 = {
   name: 'film1',
   length: 120,
-  genre: ['crime', 'drama'],
+  genres: ['crime', 'drama'],
 };
 const movieObj2 = {
   name: 'film2',
@@ -70,56 +71,14 @@ function changeAddBtnTextContent() {
     addToWatchedMovieBtn.textContent = 'Add to watched';
   }
 }
-
-//-----------------------------------
-// let queueMovieArr = [];
-// let deleteMovieBtn = document.querySelector('.add-queue-movie');
-// // функция добавления фильма в очередь
-// function onQueueBtnClick() {
-//   // добавляем объект в массив
-//   queueMovieArr.push(movieObj1);
-//   // добавляем массив в локалСторэдж и делаем строкой
-//   localStorage.setItem('queueMovieArr', JSON.stringify(queueMovieArr));
-//   // добавляем класс на кнопку
-//   addQueueMovieBtn.classList.toggle('delete-movie');
-//   const deleteMovieBtn = document.querySelector('.delete-movie');
-//   // вешаем слушатель на кнопку удвления
-//   deleteMovieBtn.addEventListener('click', delQueueMovie);
-//   // снимаем слушатель с кнопки добавления
-//   addQueueMovieBtn.removeEventListener('click', onQueueBtnClick);
-//   // меняем текстКонтент
-//   addQueueMovieBtn.textContent = 'Delete movie';
-// }
-
-// const storageParsed = JSON.parse(localStorage.queueMovieArr);
-
-// // функция удаления фильма из очереди
-// function delQueueMovie() {
-//   // перебираем массив
-//   for (let i = 0; i < storageParsed.length; i += 1) {
-//     // ищем объект для удаления
-//     if (storageParsed[i].name === movieObj1.name) {
-//       // удаляем объект из массива
-//       storageParsed.splice(i, 1);
-//       // записываем строку в локалсторэдж
-//       localStorage.queueMovieArr = JSON.stringify(storageParsed);
-//     }
-//   }
-//   // вешаем слушатель на кнопку добавления
-//   addQueueMovieBtn.addEventListener('click', onQueueBtnClick);
-//   // снимаем слушатель с кнопки удаления
-//   deleteMovieBtn.removeEventListener('click', delQueueMovie);
-//   // меняем текстконтент кнопки добавления
-//   addQueueMovieBtn.textContent = 'Add to queue';
-// }
-// localStorageQueueData === null
-//   ? (addToQueueMovieBtn.textContent = 'Add to queue')
-//   : JSON.parse(localStorageQueueData).find(el => el.name === movieObj1.name)
-//   ? (addToQueueMovieBtn.textContent = 'Delete from queue')
-//   : (addToQueueMovieBtn.textContent = 'Add to queue');
-//-------------------------------------------
-// localStorageWatchedData === null
-//   ? (addToWatchedMovieBtn.textContent = 'Add to watched')
-//   : JSON.parse(localStorageWatchedData).find(el => el.name === movieObj2.name)
-//   ? (addToWatchedMovieBtn.textContent = 'Delete from watched')
-//   : (addToWatchedMovieBtn.textContent = 'Add to watched');
+// функция примерного рендера библиотеки
+function renderLibraryMarkup(data) {
+  const localStorageData = JSON.parse(localStorage.getItem(data));
+  localStorageData.map(el => {
+    console.log(el);
+    const body = document.querySelector('body');
+    const movieCard = tamplate(el);
+    body.insertAdjacentHTML('afterbegin', movieCard);
+  });
+}
+renderLibraryMarkup('watchedMovieArr');
