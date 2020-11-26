33ac36ca087f31045b4e0f942cded06813159b9d
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-11-27 00:07:13 +0200

    changes loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,16 @@
 import '../css/loader.css';
-import refs from './refs';
+import * as basicLightbox from 'basiclightbox';
 
-export default () => refs.loader.classList.toggle('is-hidden');
+const markup = `<div id="loader">
+    <div>G</div>
+    <div>N</div>
+    <div>I</div>
+    <div>D</div>
+    <div>A</div>
+    <div>O</div>
+    <div>L</div>
+  </div>`;
+
+const loader = basicLightbox.create(markup);
+
+export default { loader };
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 export default {
   mainContent: document.querySelector('.main-js'),
   searchMovieForm: document.querySelector('.form-js'),
-  loader: document.querySelector('.loader'),
   filmInfo: document.querySelector('.lightbox_content'),
 
 };
